b83170a0329da54ef1c88dc8de659cfa08955932
Rain44556 <bristy44556@gmail.com>
2022-02-17 23:49:49 +0600

    the commit

--- js/assignment.js
+++ js/assignment.js
@@ -1,25 +1,21 @@
 function calculateButton() {
-  let income = parseFloat(document.getElementById("input-income").value);
+  const income = parseFloat(document.getElementById("input-income").value);
   const food = parseFloat(document.getElementById("input-food").value);
   const rent = parseFloat(document.getElementById("input-rent").value);
   const clothes = parseFloat(document.getElementById("input-clothes").value);
-//   income=parseFloat(income);
-//   income=parseFloat(food);
-//   income=parseFloat(rent);
-//   income=parseFloat(clothes);
 
 const savings= income - (food+rent+clothes);
 const expenses=(food + rent+ clothes);
 
-// if((typeof income==='string') || (typeof food==='string') || (typeof rent==='string') || (typeof clothes==='string') ){
-//     alert("enter a number");
-//     return;
-// }
-// if(savings<0){
+if((typeof income==='string') || (typeof food==='string') || (typeof rent==='string') || (typeof clothes==='string') ){
+    alert("enter a number");
+    return;
+}
+if(savings<0){
 
-//     alert("your expenses is greater than income");
-//      return;
-// }
+    alert("your expenses is greater than income");
+     return;
+}
 const T_Expenses=document.getElementById("tExpense");
 T_Expenses.innerText=expenses;
 console.log(T_Expenses);
@@ -27,7 +23,21 @@ console.log(T_Expenses);
 const T_Balance=document.getElementById("tBalance");
 T_Balance.innerText=savings;
 console.log(T_Balance);
+}
 
 
+function saveAmount(){
 
+    const income = parseFloat(document.getElementById("input-income").value);
+    const food = parseFloat(document.getElementById("input-food").value);
+    const rent = parseFloat(document.getElementById("input-rent").value);
+    const clothes = parseFloat(document.getElementById("input-clothes").value);
+  
+  const theAmount= income - (food+rent+clothes);
+  const sV= (theAmount/100)*10;
+  const remaining = theAmount-sV;
+  const saveAmountH= document.getElementById("Amount");
+  saveAmountH.innerText= sV;
+  const remainingBalance=document.getElementById("remaining");
+  remainingBalance.innerText= remaining;
 }
