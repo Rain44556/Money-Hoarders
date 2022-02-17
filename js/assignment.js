0a1a3d37e5f4182ff42d47ec939d36f5cf86d3af
Rain44556 <bristy44556@gmail.com>
2022-02-17 23:33:40 +0600

    1st commit

--- js/assignment.js
+++ js/assignment.js
@@ -1,13 +1,33 @@
 function calculateButton() {
-  const income = document.getElementById("input-income").value;
-  const food = document.getElementById("input-food").value;
-  const rent = document.getElementById("input-rent").value;
-  const clothes = document.getElementById("input-clothes").value;
-  
+  let income = parseFloat(document.getElementById("input-income").value);
+  const food = parseFloat(document.getElementById("input-food").value);
+  const rent = parseFloat(document.getElementById("input-rent").value);
+  const clothes = parseFloat(document.getElementById("input-clothes").value);
+//   income=parseFloat(income);
+//   income=parseFloat(food);
+//   income=parseFloat(rent);
+//   income=parseFloat(clothes);
+
 const savings= income - (food+rent+clothes);
-if(savings<0){
-    alert("your expenses is greater than income");
-     return;
-}
+const expenses=(food + rent+ clothes);
+
+// if((typeof income==='string') || (typeof food==='string') || (typeof rent==='string') || (typeof clothes==='string') ){
+//     alert("enter a number");
+//     return;
+// }
+// if(savings<0){
+
+//     alert("your expenses is greater than income");
+//      return;
+// }
+const T_Expenses=document.getElementById("tExpense");
+T_Expenses.innerText=expenses;
+console.log(T_Expenses);
+
+const T_Balance=document.getElementById("tBalance");
+T_Balance.innerText=savings;
+console.log(T_Balance);
+
+
 
 }
