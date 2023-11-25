63788d09efbd286468f5fbcdc8d75392f1bc535b
Rain44556 <bristy44556@gmail.com>
2023-11-25 12:54:19 +0600

    html file edit

--- js/calculation.js
+++ js/calculation.js
@@ -17,8 +17,8 @@ if((typeof income==='string') || (typeof food==='string') || (typeof rent==='str
     alert("enter a number");
     return;
 }
-if(savings<0){
 
+if(savings<0){
     alert("your expenses is greater than income");
      return;
 }
@@ -31,27 +31,4 @@ console.log(T_Expenses);
 const T_Balance=document.getElementById("tBalance");
 T_Balance.innerText=savings;
 console.log(T_Balance);
-}
-
-// saving and remaining
-
-
-function saveAmount(){
-
-    const income = parseFloat(document.getElementById("input-income").value);
-    const food = parseFloat(document.getElementById("input-food").value);
-    const rent = parseFloat(document.getElementById("input-rent").value);
-    const clothes = parseFloat(document.getElementById("input-clothes").value);
-  
-  const theAmount= income - (food+rent+clothes);
-  const sV= (income/100)*10;
-  if(sV>theAmount){
-alert("you can not save enough money");
- return;
-  }
-  const remaining = income-sV;
-  const saveAmountH= document.getElementById("Amount");
-  saveAmountH.innerText= sV;
-  const remainingBalance=document.getElementById("remaining");
-  remainingBalance.innerText= remaining;
 }
